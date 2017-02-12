245632d128ef950fd88491237c6ac09dc4c8a5b1
Timy90 <sy.thomas.pro@gmail.com>
2017-02-12 17:51:01 +0100

    add I18N fonctionnel

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,19 @@
 class ApplicationController < ActionController::Base
-  before_action :set_locale
+  before_action :set_i18n_locale, :set_last_url
   protect_from_forgery with: :exception
 
-  def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+  def set_i18n_locale
+    I18n.locale = cookies[:language] || I18n.default_locale
   end
-  
-protect_from_forgery
 
-  def default_url_options(options={})
-    logger.debug "default_url_options is passed options: #{options.inspect}\n"
-    { :locale => I18n.locale }
+  def set_last_url
+    session[:previous_request_url] = session[:current_request_url]
+    session[:current_request_url] = request.url
+  end
+
+  def change_language
+    cookies[:language] = { :value => params[:lang]}
+    redirect_to root_path
   end
 
 end
